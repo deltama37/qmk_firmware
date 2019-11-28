999d78d4bb06fe1d539e432527329a4d046b5b53
ryota misumi <3delta7@gmail.com>
2019-11-29 00:15:51 +0900

    コマンドキーの設定間違えてた辛い

--- keyboards/lily58/keymaps/deltama37/keymap.c
+++ keyboards/lily58/keymaps/deltama37/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |   `  | LAlt |LG,kt1| /Space  /       \Enter \  |RG,kt2|BackSP|   |  |
+ *                   |   `  | LAlt |LG,kt1| /Space  /       \Enter \  |RG,kt2|BackSP|   \  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PLUS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_GRV, KC_LALT,LT(KC_LGUI,KC_LANG2), KC_SPC,   KC_ENT,   LT(KC_RGUI,KC_LANG1),   KC_BSPC, KC_PIPE \
+                             KC_GRV, KC_LALT,LGUI(KC_LANG2), KC_SPC,   KC_ENT,   RGUI(KC_LANG1),   KC_BSPC, KC_BSLS \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
